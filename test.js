4e726c26082a1062f8bc4eebf67b3a02d524f85a
Mohit Arora <75353082+devmohitarora@users.noreply.github.com>
2026-06-09 08:48:15 +0530

    changes

--- test.js
+++ test.js
@@ -1,30 +1,23 @@
-const abc =()=>{
-    console.log("hello world")
-}
+const abc = () => {
+  console.log("hello world");
+};
 
-const abc1 =()=>{
-    console.log("hello world")
-}
+const abc1 = () => {
+  console.log("hello world");
+};
 
+const abc2 = () => {
+  console.log("hello world");
+};
 
-const abc2 =()=>{
-    console.log("hello world")
-}
+const abc3 = () => {
+  console.log("hello world");
+};
 
+const abc4 = () => {
+  console.log("hello world");
+};
 
-const abc3 =()=>{
-    console.log("hello world")
-}
-
-
-
-
-
-
-
-
-
-
-const abc5 =()=>{
-    console.log("hello world")
-}
+const abc5 = () => {
+  console.log("hello world");
+};
